f0be356407a1214c945d52371924431f4bd53e2f
Fabricio <fabriciosilvalp@outlook.com>
2025-12-07 23:27:08 -0300

    Refactor: Split IAuthServices into IPasswordHasher and ITokenService

--- src/StreamForge.Application/Interfaces/ITokenService.cs
+++ src/StreamForge.Application/Interfaces/ITokenService.cs
@@ -2,12 +2,6 @@ using StreamForge.Domain.Entities;
 
 namespace StreamForge.Application.Interfaces;
 
-public interface IPasswordHasher
-{
-    string Hash(string password);
-    bool Verify(string password, string hash);
-}
-
 public interface ITokenService
 {
     string GenerateToken(User user);
